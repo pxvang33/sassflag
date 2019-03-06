6560c9de900521dbca4e1162ee427c4325d3c36f
RobertAKARobin <robertgfthomas@gmail.com>
2019-03-06 14:01:47 -0600

    Added gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@ const {src, dest, watch} = require('gulp')
 const sass = require('gulp-sass')
 
 const build = function(){
-	return
+	return src('./src/main.scss')
+	.pipe(sass({outputStyle: 'expanded'}))
+	.pipe(dest('./dist'))
 }
 
 const watchAndBuild = function(){
-	return
+	return watch([
+		'./src/*.scss'
+	], {ignoreInitial: false}, build)
 }
+
+exports.build = build
+exports.watch = watchAndBuild
